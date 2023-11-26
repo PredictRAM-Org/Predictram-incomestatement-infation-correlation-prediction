bd5493c8642b59456c5d5297b4f1e91f8fa75a61
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-11-27 02:31:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_squared_error
 import seaborn as sns
 
 # Load CPI data
-cpi_data = pd.read_csv('CPI.csv')
+cpi_data = pd.read_csv('CPI.csv')  # Replace 'CPI.csv' with the correct path to your CPI data file
 cpi_data['Date'] = pd.to_datetime(cpi_data['Date'])
 cpi_data.set_index('Date', inplace=True)
 
@@ -19,52 +19,64 @@ st.title('Inflation and Financial Data Analysis')
 uploaded_file = st.file_uploader("Upload Quarterly Financial Data File", type=["xlsx", "xls"])
 
 if uploaded_file is not None:
-    # Read uploaded file
-    financial_data = pd.read_excel(uploaded_file, index_col=0)
+    try:
+        # Read uploaded file
+        financial_data = pd.read_excel(uploaded_file, index_col=0)
 
-    # Merge data based on the common date index
-    merged_data = pd.merge(financial_data, cpi_data, how='inner', left_index=True, right_index=True)
+        # Merge data based on the common date index
+        merged_data = pd.merge(financial_data, cpi_data, how='inner', left_index=True, right_index=True)
 
-    # Calculate correlations
-    correlation_data = merged_data[['Total Revenue/Income', 'Total Operating Expense', 'Income/Profit Before Tax', 'Net Income', 'Inflation']]
-    correlation_matrix = correlation_data.corr()
+        # Print column names for debugging
+        st.write("Merged Data Columns:", merged_data.columns)
 
-    # Plot and save correlation matrix heatmap
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
-    plt.title('Correlation Matrix')
-    plt.savefig('correlation_matrix.png')
-    st.image('correlation_matrix.png')
+        # Check if the specified columns exist in the DataFrame
+        required_columns = ['Total Revenue/Income', 'Total Operating Expense', 'Income/Profit Before Tax', 'Net Income', 'Inflation']
+        if all(column in merged_data.columns for column in required_columns):
+            # Calculate correlations
+            correlation_data = merged_data[required_columns]
+            correlation_matrix = correlation_data.corr()
 
-    # Regression analysis
-    features = ['Inflation']
-    target_columns = ['Total Revenue/Income', 'Net Income']
+            # Plot and save correlation matrix heatmap
+            plt.figure(figsize=(10, 8))
+            sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+            plt.title('Correlation Matrix')
+            plt.savefig('correlation_matrix.png')
+            st.image('correlation_matrix.png')
 
-    for target_col in target_columns:
-        X = merged_data[features]
-        y = merged_data[target_col]
+            # Regression analysis
+            features = ['Inflation']
+            target_columns = ['Total Revenue/Income', 'Net Income']
 
-        # Split data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            for target_col in target_columns:
+                X = merged_data[features]
+                y = merged_data[target_col]
 
-        # Create and train the model
-        model = LinearRegression()
-        model.fit(X_train, y_train)
+                # Split data into training and testing sets
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Make predictions
-        y_pred = model.predict(X_test)
+                # Create and train the model
+                model = LinearRegression()
+                model.fit(X_train, y_train)
 
-        # Calculate and display Mean Squared Error
-        mse = mean_squared_error(y_test, y_pred)
-        st.write(f'Mean Squared Error for {target_col}: {mse:.2f}')
+                # Make predictions
+                y_pred = model.predict(X_test)
 
-        # Plot and save regression line
-        plt.figure(figsize=(8, 6))
-        plt.scatter(X_test, y_test, color='blue', label='Actual')
-        plt.plot(X_test, y_pred, color='red', linewidth=2, label='Predicted')
-        plt.title(f'Regression Analysis for {target_col}')
-        plt.xlabel('Inflation')
-        plt.ylabel(target_col)
-        plt.legend()
-        plt.savefig(f'regression_{target_col.lower().replace(" ", "_")}.png')
-        st.image(f'regression_{target_col.lower().replace(" ", "_")}.png')
+                # Calculate and display Mean Squared Error
+                mse = mean_squared_error(y_test, y_pred)
+                st.write(f'Mean Squared Error for {target_col}: {mse:.2f}')
+
+                # Plot and save regression line
+                plt.figure(figsize=(8, 6))
+                plt.scatter(X_test, y_test, color='blue', label='Actual')
+                plt.plot(X_test, y_pred, color='red', linewidth=2, label='Predicted')
+                plt.title(f'Regression Analysis for {target_col}')
+                plt.xlabel('Inflation')
+                plt.ylabel(target_col)
+                plt.legend()
+                plt.savefig(f'regression_{target_col.lower().replace(" ", "_")}.png')
+                st.image(f'regression_{target_col.lower().replace(" ", "_")}.png')
+
+        else:
+            st.write("Error: One or more required columns are missing.")
+    except Exception as e:
+        st.write(f"Error: {e}")
